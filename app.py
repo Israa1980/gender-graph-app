d3670874f5c8febbd1a059f742996bf792b1b5a7
Israa1980 <91760832+Israa1980@users.noreply.github.com>
2025-11-14 14:36:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -140,9 +140,8 @@ CLASS_DESCRIPTIONS = {
                "Your visualisation is considered **inclusive for males** as it uses one of the following colour harmony techniques: "
                 "Complementary colour strategy (base colour dominant), complementary colour strategy (complementary colour dominant), "
                 "monochromatic strategy (base colour dominant), or analogous strategy (analogous colour dominant), which our studies suggest "
-                "is generally inclusive for male users. To make it inclusive for both genders, try strategies like split complementary or "
-                "three-colour analogous schemes. For example, you could use split complementary colour harmony strategies (three complementary colours), "
-                "an analogous strategy with three analogous colours, or an analogous strategy with two analogous colours where the base colour is dominant."
+                "is generally inclusive for male users. "To improve inclusivity, you could adopt strategies such as split complementary colour harmony (three complementary colours), "
+                "an analogous strategy using three analogous colours, or an analogous strategy with two analogous colours where the base colour is dominant."
     ),
        "inclusive for both genders": (
         " Well done! Your data visualisation is considered **inclusive for both genders** as it uses one of the following colour harmony techniques:\n"
